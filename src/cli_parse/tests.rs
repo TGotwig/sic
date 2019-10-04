676429fc16af9287d9d552c7a7afbd3d9b9d9e91
foresterre <garm@ilumeo.com>
2019-10-04 18:22:52 +0200

    Add sigil to test for failure cases.

--- src/cli_parse/tests.rs
+++ src/cli_parse/tests.rs
@@ -4,7 +4,19 @@ use std::path::{Path, PathBuf};
 #[derive(Debug)]
 struct TestFile {
     image_path: PathBuf,
-    cases: Vec<String>,
+    cases: Vec<Case>,
+}
+
+#[derive(Debug)]
+struct Case {
+    line: String,
+    expected_outcome: Outcome,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Outcome {
+    Success,
+    Failure,
 }
 
 fn load_test_files() -> Vec<TestFile> {
@@ -18,7 +30,7 @@ fn load_test_files() -> Vec<TestFile> {
             if let Some(ext) = path.as_ref().unwrap().path().extension() {
                 if ext == "sic" {
                     let cases = path.as_ref().unwrap().path();
-                    return Some(load_test_file(&cases))
+                    return Some(load_test_file(&cases));
                 }
             }
             None
@@ -33,11 +45,19 @@ fn load_test_file(path: &Path) -> TestFile {
             .filter_map(|line| {
                 if line.starts_with("#") {
                     None
+                } else if line.starts_with("@") {
+                    Some(Case {
+                        line: line[1..].to_string(), // lets hope we don't ever need to deal with wonky unicode here, because we slice on bytes <3
+                        expected_outcome: Outcome::Failure,
+                    })
                 } else {
-                    Some(String::from(line))
+                    Some(Case {
+                        line: String::from(line),
+                        expected_outcome: Outcome::Success,
+                    })
                 }
             })
-            .collect::<Vec<String>>()
+            .collect::<Vec<Case>>()
     });
 
     return TestFile {
@@ -49,16 +69,23 @@ fn load_test_file(path: &Path) -> TestFile {
 fn run_test(test_file: &TestFile) {
     for case in &test_file.cases {
         let input = case
+            .line
             .split_ascii_whitespace()
             .map(String::from)
             .collect::<Vec<_>>()
             .into_iter();
 
-        let out = prototype(input).unwrap();
+        let out = prototype(input);
 
-        println!("optree: {:?}\n", out);
-
-        assert!(out.len() > 0);
+        match case.expected_outcome {
+            Outcome::Success => {
+                assert!(out.is_ok());
+                let out = out.unwrap();
+                println!("optree: {:?}\n", out);
+                assert!(out.len() > 0);
+            }
+            Outcome::Failure => assert!(out.is_err()),
+        }
     }
 }
 
@@ -81,7 +108,11 @@ fn all() {
 
 #[test]
 fn multi_arg() {
-    let path = &[env!("CARGO_MANIFEST_DIR"), "/resources/tests-parser/arg_modifiers.sic"].concat();
+    let path = &[
+        env!("CARGO_MANIFEST_DIR"),
+        "/resources/tests-parser/arg_modifiers.sic",
+    ]
+    .concat();
     let path = Path::new(path);
     let test_file = load_test_file(path);
     run_test(&test_file);
